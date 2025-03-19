d847f1e7ccee859d26348cf0e4d50fbc08a58222
fic <fic@NOTE04-S28>
2025-03-19 19:09:32 -0300

    aula d18.03

---
+++ Curso-SENAI02/Aula.cs
@@ -0,0 +1,230 @@
+﻿////              fundamentos do C#
+
+//// toda linha termina com ';'
+//// <tipo do dado> <nome> = <valor inicial>
+//// = - atribuicao
+
+//// entrada e saida
+//// operadores
+//// constantes e variaveis
+
+//// mostra coisas no console (saida)
+//Console.Write("informe um usuario: ");
+//// manda coisas para o console (entrada)
+//string usuario = Console.ReadLine();
+////ReadLine so le texto para usar numero, precisa converter (Parse converte string para int ou double)
+//Console.Write("insira um numero: ");
+//int data = int.Parse(Console.ReadLine());
+////concatenacao
+//Console.WriteLine("Ola, seja bem vindo: " + usuario);
+////interpolacao
+//Console.WriteLine($"Ola, seja bem vindo: {usuario}");
+
+//// NUMEROS
+
+//// variavel - o valor pode mudar
+//// so guardar como numero aquilo que vai usar para fazer calculo
+//// int - numero inteiro
+//int idade = 18;
+//idade = 22;
+//idade = 23;
+//Console.WriteLine(idade);
+
+////constante - o valor nao muda
+//const int novaidade = 24;
+////novaidade = 26;  (comentado so pra nao ficar com erros no codigo)
+//Console.WriteLine(novaidade);
+
+//// double - numeros quebrados
+//double preco = 9.99;
+
+//// float - numeros quebrados (aceita menos casas decimais)(usa menos memoria)(obrigatorio usar "f" no final)
+//float altura = 1.80f;
+
+//// bool - false ou true (condicao)
+//bool careca = false;
+
+//// LETRAS
+
+//// string - qualquer caractere
+//string texto = "is Ward9012 BB!";
+
+//// char - para guardar 1 cactere
+//char letra = 'B';
+
+//string nome = "ward";
+//string novonome = nome;
+
+//int numero1 = 1;
+//int numero2 = 2;
+
+//int resultado = numero1 + numero2;
+
+//// Operadores
+
+//// Operadores aritimeticos
+
+//// + - soma ou concatena (juntar texto)
+
+//Console.WriteLine(5 + 4);
+
+//Console.WriteLine(5 + "5");
+
+//// - - subtracao
+//Console.WriteLine(4 - 5);
+
+//// * - multiplicacao
+//Console.WriteLine(5 * 7);
+
+//// / - divisao
+//Console.WriteLine(7.0 / 3);
+
+//// 1 - parenteses
+//// 2 - multiplicacao e divisao
+//// 3 - soma e subtracao
+//Console.WriteLine((4 - 9.0) / 2);
+
+//// modulo - da o resto da divisao
+//Console.WriteLine(5 % 2);
+
+
+//// Operadores de comparacao
+
+//// == - Igual
+//Console.WriteLine(numero1 == numero2);
+
+//// != - diferente 
+//Console.WriteLine(5 != 6);
+
+//// > - maior
+//Console.WriteLine(5 > 6);
+
+//// >= - maior ou igual
+//Console.WriteLine(5 >= 4);
+
+//// < - menor 
+//Console.WriteLine(7 < 9);
+
+//// <= menor ou igual
+//Console.WriteLine(7 <= 11);
+
+////Estruturas Condicionais(if e else)
+
+//Console.Write("informe sua idade: ");
+//int idadeif = int.Parse(Console.ReadLine());
+
+//// condicional simples
+//if (idadeif > 18)
+//{
+//    Console.WriteLine("vc e maior de idade!");
+//}
+//else
+//{
+//    Console.WriteLine("vc e menor de idade!");
+//}
+
+//// operador ternário (if else simplificado)
+//int idadeternario = 20;
+//string msg = (idadeternario > 18) ? "maior de idade" : "menor de idade";
+
+//// condicional encadeada
+//if (idadeif > 17)
+//{
+//    Console.WriteLine("adulto");
+//}
+//else if (idadeif > 12)
+//{
+//    Console.WriteLine("adolescente");
+//}
+//else
+//{
+//    Console.WriteLine("crianca");
+//}
+
+//// operador e - &&
+//// operador ou - ||
+//bool autorizaPais = true;
+//if (idadeif > 16 && autorizaPais == true)
+//{
+//    Console.WriteLine("pode votar!");
+//}
+
+//// estrturas de repeticao <repete uma acao especifica>
+//// imprima todos os numeros de 1 a 5
+
+//// while - enquanto <repete uma acao ENQUANTO algo for verdade>
+//int numero = 1;
+//while (numero <= 5)
+//{
+//    Console.WriteLine(numero);
+//    // numero = numero + 1; <comentado pois nao precisa de 2 comandos iguais>
+//    // comando abaixo igual o de cima, porem, e um atalho
+//    numero++;
+//}
+
+//// Adivinhe a minha idade
+
+//// Do/while - faca
+
+////exemplo usando o while
+//Console.WriteLine("informe a idade");
+//int idade = int.Parse(Console.ReadLine());
+
+//while (idade != 20)
+//{
+//    Console.WriteLine("informe a idade");
+//    idade = int.Parse(Console.ReadLine());
+//}
+
+//// exemplo com  o do/while
+//// precisa criar a variavel fora das cahves, pois se criar dentro das chaves ela nao pode ser usado fora
+//int idade;
+//do
+//{
+//    Console.WriteLine("informe a idade");
+//    idade = int.Parse(Console.ReadLine());
+//} while (idade != 20);
+
+//// for - para
+////quero imprimir os numeros de 1 a 5
+//// <cria uma variavel chamada i (indice)> <condicao para continuar> <incremento ao usar a chave
+//for (int i = 0; i < 6; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+// Tratamento de erros
+
+//int numero = int.Parse(Console.ReadLine());
+
+// Exception = erro
+// IOExeption - In and Out - Entrada e Saida
+// FormatException - Erros no formato (tipo de dado)
+// OverflowException - variavel transbordou
+
+// try/catch - Tentar/Pegar
+//
+//try
+//{
+//    int num = int.Parse(Console.ReadLine());
+//}
+//catch (FormatException ex)
+//{
+//    Console.WriteLine("Erro: informe apenas numeros");
+//}
+//catch (OverflowException ex)
+//{
+//    Console.WriteLine("Erro: numero muito grande");
+//}
+////finaliza programas que deram erro
+//finally
+//{
+
+//}
+
+//int idade  = int.Parse(Console.ReadLine());
+////cria um erro
+//if (idade >120)
+//{
+//    throw new ArgumentOutOfRangeException("Idade invalida");
+//}
---
+++ Execercicios/Exercicios.cs
@@ -0,0 +1,168 @@
+﻿////          Excercicios 12/03
+
+//// exerciccio 1
+//using System.Reflection.PortableExecutable;
+
+//Console.WriteLine(54 + 54);
+//// exerciccio 2
+//Console.WriteLine(5 * 5);
+//// exerciccio 3
+//Console.WriteLine(4 - 2);
+//// exerciccio 4
+//Console.WriteLine((6 + 6 + 6) / 3);
+//// exerciccio 5
+//Console.WriteLine(8 % 2);
+
+////         Excercicios 13/03
+
+//// exrcicio 1
+//Console.WriteLine("escreva dois numeros para eu lhe dizer qual deles é maior: ");
+//int numero1 = int.Parse(Console.ReadLine());
+//int numero2 = int.Parse(Console.ReadLine());
+
+//if (numero1 > numero2)
+//{
+//    Console.WriteLine("O primeiro é maior");
+//}
+//else if (numero1 < numero2)
+//{
+//    Console.WriteLine("O segundo é maior");
+//}
+//else
+//{
+//    Console.WriteLine("Os dois são iguais");
+//}
+
+//// exercicio 2
+//Console.WriteLine("informe a sua idade: ");
+//int idade = int.Parse(Console.ReadLine());
+
+//if (idade > 18)
+//{
+//    Console.WriteLine("Você é adulto!");
+//}
+//else if (idade > 13)
+//{
+//    Console.WriteLine("Você é adolescente!");
+//}
+//else
+//{
+//    Console.WriteLine("Você é crianca!");
+//}
+
+//// excercicio 3
+//Console.WriteLine("escreva um numero e eu lhe direi se e par ou impar!");
+//int numero = int.Parse(Console.ReadLine());
+
+//if (numero % 2 == 1)
+//{
+//    Console.WriteLine("É impar!");
+//}
+//else
+//{
+//    Console.WriteLine("É par!");
+//}
+
+//// excercicio 4
+//Console.WriteLine("escreva tres numeros e irei lhe dizer qual deles é o maior!!!");
+//int num1 = int.Parse(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+//int num3 = int.Parse(Console.ReadLine());
+
+//if (num1 > num2 && num1 > num3)
+//{
+//    Console.WriteLine("O primeiro é maior");
+//}
+//else if (num2 > num1 && num2 > num3)
+//{
+//    Console.WriteLine("O segundo é maior");
+//}
+//else if (num3 > num1 && num3 > num2)
+//{
+//    Console.WriteLine("O terceiro e maior");
+//}
+//else
+//{
+//    Console.WriteLine("Os tres são iguais");
+//}
+
+//// excercicio 5
+//Console.WriteLine("Me entregue 3 numeros e euirei resolver bhaskara!!!");
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+
+//int delta = b * b - (4 * a * c);
+
+//double raiz1 = (-b + Math.Sqrt(delta)) / (2 * a);
+//double raiz2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+//if (delta > 0)
+//{
+//    Console.WriteLine($"As raizes são: {raiz1} e {raiz2}");
+//}
+//else
+//{
+//    Console.WriteLine("Não existe raizes rais para esse calculo");
+//}
+
+//// EXERCICIOS (17/03/2022)
+
+//// Exercicio 01 (conte de 1 a 20)
+//int numero = 0;
+//do
+//{
+//    Console.WriteLine(numero);
+//    numero++;
+//} while (numero <= 20);
+
+//// exercicio 02 (valide a senha 1234, continue pedindo ate que acerte)
+//Console.WriteLine("acerte a senha");
+//int senha = int.Parse(Console.ReadLine());
+
+//while (senha != 1234)
+//{
+//    Console.WriteLine("tente dnv");
+//    senha = int.Parse(Console.ReadLine());
+//}
+
+//// exercicio 03 (tauada, peca ao usuario um numero e exiba a tabuada de 1 a 10)
+
+//Console.WriteLine("fale um numero e receba a sua tabuada");
+//int tabuada = int.Parse(Console.ReadLine());
+//int numerador = 1;
+//while (numerador < 10)
+//{
+//    numerador++;
+//    Console.WriteLine($"{tabuada} x {numerador} = {tabuada * numerador}");
+//}
+
+//// Exercicio 4 (contagem regressiva, peca um numero ao usuario e  realize uma contagem regressiva ate 0)
+
+//Console.WriteLine("me entrege um numero que eu realizarei a contagem regressiva");
+//int numero = int.Parse(Console.ReadLine());
+
+//do
+//{
+//    Console.WriteLine(numero--);
+//} while (numero > 0);
+
+//// exercicio 5 (media de notas, Solicite notas ao usuário (entre 0 e 10) até que ele insira um número inválido. Depois, exiba a média das notas)
+
+//double notas;
+//double SomaNotas = 0.0;
+//double qtdNotas = 0.0;
+
+//do
+//{
+//    Console.Write("informe uma nota: ");
+//    notas = int.Parse(Console.ReadLine());
+//    if (notas >= 0 && notas <= 10)
+//    {
+//        SomaNotas = notas + SomaNotas;
+//        qtdNotas++;
+//    }
+//} while (notas >= 0 && notas <= 10);
+
+//double media = SomaNotas / qtdNotas;
+//Console.WriteLine("a media e: " + media);
